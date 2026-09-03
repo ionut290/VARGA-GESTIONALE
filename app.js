39ac35cc0dc890ac17366aa21b974f76127748f7
ionut290 <148634773+ionut290@users.noreply.github.com>
2026-09-03 19:12:27 +0200

    Add stable application bootstrap

---
+++ app.js
@@ -0,0 +1,3 @@
+// Bootstrap stabile di Varga Gestionale.
+// I moduli sotto vengono aggiornati in-place tramite commit GitHub; non vengono creati file versione-v2/v3 ad ogni modifica.
+document.write('<script src="app-core.js"><\/script><script src="app-business.js"><\/script><script src="app-varga.js"><\/script><script src="app-sync.js"><\/script><script src="app-cloud.js"><\/script>');
